cab9e91bfd8bd5f69784ae6092a8fecb8ba98fe3
HarshSharma018 <harshjangid033@gmail.com>
2026-07-19 00:12:30 +0530

    auth concepts

--- auth.js
+++ auth.js
@@ -9,3 +9,19 @@ const server = http.createServer((req, res) => {
   const token = authHeader.split(' ')[1]; // 'Bearer <token>' -> take the token
   res.end(JSON.stringify({ token }));
 });
+
+//cors snippet
+/*res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');*/
+
+
+/*1)Access-Control-Allow-Origin: '*' — tells the browser "any frontend domain can call this API." In 
+production you'd replace * with your actual frontend URL (e.g. https://myapp.com) for security —
+ * is fine for learning/dev only.
+
+2)Access-Control-Allow-Methods — whitelist of HTTP methods the frontend is allowed to use
+ against this server.
+
+3)Access-Control-Allow-Headers — whitelist of which headers the frontend is allowed 
+to send (so if your frontend sends a custom header not listed here, browser blocks it too).*/
