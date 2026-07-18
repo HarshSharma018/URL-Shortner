a4e03125d6477c97a7f244a700759bb106fa1621
HarshSharma018 <harshjangid033@gmail.com>
2026-07-18 23:53:03 +0530

    auth

---
+++ auth.js
@@ -0,0 +1,9 @@
+const server = http.createServer((req, res) => {
+  const authHeader = req.headers['authorization'];
+  if (!authHeader) {
+    res.statusCode = 401;
+    return res.end(JSON.stringify({ error: 'No token provided' }));
+  }
+  const token = authHeader.split(' ')[1]; // 'Bearer <token>' -> take the token
+  res.end(JSON.stringify({ token }));
+});
