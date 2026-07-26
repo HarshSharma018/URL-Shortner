92098c8c8c5a550d622e6c0470e97f42bdba948e
HarshSharma018 <harshjangid033@gmail.com>
2026-07-26 23:50:49 +0530

    route users

--- route.js
+++ route.js
@@ -14,7 +14,22 @@ const server = http.createServer((req, res) => {
 
 server.listen(3000);*/
 
-app.get('/users/:id', (req, res) => {
-  console.log(req.params); // { id: '42' }
-  res.json({ userId: req.params.id });
+//app.get('/users/:id', (req, res) => {
+//  console.log(req.params); // { id: '42' }
+//  res.json({ userId: req.params.id });
+//});
+
+/* order of routine 
+app.get('/users/new', (req, res) => { /* create-user form 
+app.get('/users/:id', (req, res) => { /* get user by id });
+*/
+
+/*Use route params for things required to identify the resource: /products/:id
+Use query params for optional filters/modifiers on that resource: /products/:id?fields=name,price*/
+
+app.use(express.json()); // MUST come before your routes — parses JSON body
+
+app.post('/users', (req, res) => {
+  const { name, email } = req.body;
+  res.status(201).json({ message: 'User created', name, email });
 });
